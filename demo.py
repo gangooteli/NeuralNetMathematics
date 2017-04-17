cf5d85013a58d47d130daad5d5014ba0f2fb7a2e
Nikhil Kumar Gupta29 <Nikhil_Gupta30@ad.infosys.com>
2017-04-17 17:52:07 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+#collect data
+
+x = np.array([[0,0,1],
+		[0,1,1],
+		[1,0,1],
+		[1,1,1]])
+
+y = np.array([[0],
+		[1],
+		[1],
+		[0]])
+
+#build model
+num_epochs = 60000
+
+#intialize weight
+syn0 = 2*np.random.random((3,4))-1
+syn1 = 2*np.random.random((4,1))-1
+
+print x
+print y
+print syn0
+print syn1
+
+def nonlin(x, deriv = False):
+	if(deriv == True):
+		return x*(1-x)
+	return 1/(1+np.exp(-x))
+
+#step 3 
+for j in xrange(num_epochs):
+	#feed forward through layers 0, 1 and 2
+	l0 = x
+	l1 = nonlin(np.dot(l0, syn0))
+	l2 = nonlin(np.dot(l1, syn1))
+
+	#how de we optimise
+	l2_error = y-l2
+	
+	if(j%10000) ==0:
+		print("Error:"+str(np.mean(np.abs(l2_error))))
+	
+	#derivative
+	l2_delta = l2_error*nonlin(l2, deriv=True)
+	
+	#how much did each l1 value contribute to l2 error
+	l1_error = l2_delta.dot(syn1.T)
+	l1_delta = l1_error * nonlin(l1, deriv = True)
+	
+	syn1 += l1.T.dot(l2_delta)
+	syn0 += l0.T.dot(l1_delta)
+
+
+
+
+
+
